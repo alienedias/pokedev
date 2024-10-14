7dd2c1494678920c2ba0e0db325c2eda874d8cc0
aliene <alienedias@gmail.com>
2024-10-14 11:50:05 -0300

    refactor: :recycle: adding functions structure

--- src/script.js
+++ src/script.js
@@ -1,28 +1,36 @@
-//PASSO 1
 const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
 
-//PASSO 2
 listaSelecaoPokedevs.forEach((pokedev) => {
   pokedev.addEventListener("click", () => {
-    //PASSO 3
-    const cartaoPokedevAberto = document.querySelector(".aberto");
-    cartaoPokedevAberto.classList.remove("aberto");
-
-    //PASSO 4
-    const idPokedevSelecionado = pokedev.attributes.id.value;
-    const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
-    const cartaoPokedevParaAbrir = document.getElementById(
-      idDoCartaoPokedevParaAbrir
-    );
-    cartaoPokedevParaAbrir.classList.add("aberto");
-
-    //PASSO 5
-    const pokedevNaListagem = document.querySelector(".ativo");
-    pokedevNaListagem.classList.remove("ativo");
-
-    //PASSO 6
-    const pokedevSelecionadoNaListagem =
-      document.getElementById(idPokedevSelecionado);
-    pokedevSelecionadoNaListagem.classList.add("ativo");
+    esconderCartaoPokedev();
+    const idPokedevSelecionado = mostrarCartaoPokedevSelecionado(pokedev);
+    desativarPokedevNaListagem();
+    ativarPokedevNaListagem(idPokedevSelecionado);
   });
 });
+
+function ativarPokedevNaListagem(idPokedevSelecionado) {
+  const pokedevSelecionadoNaListagem =
+    document.getElementById(idPokedevSelecionado);
+  pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desativarPokedevNaListagem() {
+  const pokedevNaListagem = document.querySelector(".ativo");
+  pokedevNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokedevSelecionado(pokedev) {
+  const idPokedevSelecionado = pokedev.attributes.id.value;
+  const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+  const cartaoPokedevParaAbrir = document.getElementById(
+    idDoCartaoPokedevParaAbrir
+  );
+  cartaoPokedevParaAbrir.classList.add("aberto");
+  return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+  const cartaoPokedevAberto = document.querySelector(".aberto");
+  cartaoPokedevAberto.classList.remove("aberto");
+}
